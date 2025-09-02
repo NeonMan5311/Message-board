555a11c05562d20d6e139475321b0de6e4ec7ba5
NeonMan5311 <dhengleyadav@gmail.com>
2025-09-02 23:01:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import express from "express";
 import path from "path"
-import livereload from "livereload";
-import connectLiveReload from "connect-livereload";
+
 const app=express();
 const __dirname = import.meta.dirname;
-const PORT=3000;
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-	setTimeout(() => {
-		liveReloadServer.refresh("/");
-	}, 3000);
-});
-app.use(connectLiveReload());
+
+
 
 app.use(express.urlencoded({ extended: true }));
 import {indexRouter} from "./routes/indexRouter.js"
@@ -22,7 +15,4 @@ app.use(express.static(path.join(__dirname, "src")));
 
 app.use("/",indexRouter)
 
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`);
-})
-
+export default app;
